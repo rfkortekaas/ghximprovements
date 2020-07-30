a290aefb8080fe0f414b09efb84c7b5ea48b588c
R.F. Kortekaas <rfkortekaas@users.noreply.github.com>
2020-07-30 09:57:09 +0200

    Update GHX Improvements.user.js
    Forward to new url.

--- GHX Improvements.user.js
+++ GHX Improvements.user.js
@@ -4,11 +4,12 @@
 // @homepage     https://gist.github.com/rfkortekaas
 // @description  Improve GHX EBS
 // @author       @rfkortekaas
-// @match        https://ebs.ghx.com/*/nw_overview.cfm*
-// @match        https://ebs.ghx.com/*/index.cfm*
-// @match        https://ebs.ghx.com/*/nw_createReq.cfm*
-// @match        https://ebs.ghx.com/*/nw_supplierInfo.cfm*
-// @match        https://ebs.ghx.com/*/login.cfm*
+// @match        https://surfnet-ebs.ghx.com/*/nw_overview.cfm*
+// @match        https://surfnet-ebs.ghx.com/*/index.cfm*
+// @match        https://surfnet-ebs.ghx.com/*/nw_createReq.cfm*
+// @match        https://surfnet-ebs.ghx.com/*/nw_supplierInfo.cfm*
+// @match        https://surfnet-ebs.ghx.com/*/login.cfm*
+// @match        https://ebs.ghx.com/*
 //
 // @resource     Select2 https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
@@ -30,10 +31,13 @@
             document.title = "GHX Improved 0.3";
 
             if (window.location.href.indexOf("login") > -1) {
-                window.location.href = "https://ebs.ghx.com/synqeps/webroot/login_UVAHVA.cfm?skin=ghx/";
+                window.location.href = "https://surfnet-ebs.ghx.com/synqeps/webroot/login_UVAHVA.cfm?skin=ghx/";
             }
             else if (window.location.href.indexOf("index") > -1) {
-                window.location.href = "https://ebs.ghx.com/synqeps/webroot/login_UVAHVA.cfm?skin=ghx/";
+                window.location.href = "https://surfnet-ebs.ghx.com/synqeps/webroot/login_UVAHVA.cfm?skin=ghx/";
+            }
+            else if (window.location.href.indexOf("https://ebs") > -1) {
+                window.location.href = "https://surfnet-ebs.ghx.com/synqeps/webroot/login_UVAHVA.cfm?skin=ghx/";
             }
             else if (window.location.href.indexOf("nw_createReq") > -1) {
                 $(function () {
