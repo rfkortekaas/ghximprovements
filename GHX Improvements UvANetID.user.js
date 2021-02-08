4646849d4de8def7e2cc1fe32e16029c0d17e3fb
R.F. Kortekaas <rfkortekaas@users.noreply.github.com>
2021-02-08 16:28:41 +0100

    Update GHX Improvements UvANetID.user.js

--- GHX Improvements UvANetID.user.js
+++ GHX Improvements UvANetID.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         GHX Improvements UvANetId
-// @version      0.4
-// @homepage     https://gist.github.com/rfkortekaas
+// @version      0.7
+// @homepage     https://github.com/rfkortekaas/ghximprovements/blob/master/GHX Improvements UvANetID.user.js?raw=true
 // @description  Improve GHX EBS
 // @author       @rfkortekaas
 // @match        https://surfnet-ebs.ghx.com/*/nw_overview.cfm*
 // @match        https://surfnet-ebs.ghx.com/*/nw_createReq.cfm*
 // @match        https://surfnet-ebs.ghx.com/*/nw_supplierInfo.cfm*
 // @match        https://surfnet-ebs.ghx.com/*/nw_searchOrderReceipt.cfm*
+// @match        https://ebs.ghx.com/*
 //
 // @resource     Select2 https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
@@ -18,6 +19,23 @@
 // @run-at       document-start
 // ==/UserScript==
 
+GM_addStyle ( `
+    .inputcount,
+    #ghx-i-hide{
+        font:inherit;
+        margin:0;
+        padding:0;
+    }
+    .inputcount{
+        border:none;
+        min-width:10px;
+    }
+    .ghx-i-hide{
+        display:none;
+        white-space:pre;
+    }
+` );
+
 (function() {
     'use strict';
     var Select2 = GM_getResourceText("Select2");
@@ -26,9 +44,12 @@
     jQuery.noConflict();
     (function( $ ) {
         $(function() {
-            document.title = "GHX Improved 0.4 UvANetId";
+            document.title = "GHX Improved 0.7 UvANetId";
 
-            if (window.location.href.indexOf("nw_createReq") > -1) {
+            if (window.location.href.indexOf("https://ebs") > -1) {
+                window.location.href = "https://www.uva.nl/orderdirect";
+            }
+            else if (window.location.href.indexOf("nw_createReq") > -1) {
                 $(function () {
                     $("select").select2();
                 });
@@ -56,14 +77,20 @@
                     $("select").select2();
                 });
             }
-            else {
-                var els = document.getElementsByClassName('inputcount'), elLength = els.length;
+            else if (window.location.href.indexOf("nw_overview") > -1) {
+                $(".inputcount").each(function ( index ) {
+                    $($(".inputcount")[index]).before('<span class="ghx-i-hide '+index+'"></span>');
+                });
 
-                for (var i = 0; i < elLength; i++) {
-                    els.item(i).removeAttribute('readonly');
-                };
+                setInterval(function(){
+                    $(".inputcount").each(function ( index ) {
+                        $('.ghx-i-hide.'+index).text($($(".inputcount")[index]).val());
+                        $($(".inputcount")[index]).width($('.ghx-i-hide.'+index).width());
+                    });
+                }, 100);
             }
         });
     })(jQuery);
 
 })();
+
